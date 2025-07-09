45c6ac6092eb9372dfa8df2f2a36092dcbd6744b
Alexis Acosta Fonfria <alexis.acostafonfria@gmail.com>
2025-07-09 20:19:51 +0000

    sigue emitiendo el mismo error al compilar

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('mysql2');
+    }
+    return config;
+  },
   allowedDevOrigins: ['https://3000-firebase-studio-1749163371503.cluster-76blnmxvvzdpat4inoxk5tmzik.cloudworkstations.dev'],
 };
 
